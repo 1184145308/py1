7ebdd44166f8b071770dc32e7277761591b68d3b
猥琐兔子 <18268893871@163.com>
2017-09-17 17:45:51 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+# py1
+#-*-encoding:utf-8-*-
+#服务器
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(('127.0.0.1',65534))
+s.listen(5)
+print('waiting for connecting.....')
+
+while True:
+    sock,addr=s.accept()
+    print('New connecting from %s' %addr)
+    sock.send(b'Welcome!')
+    while True:
+        data=sock.recv(1024)
+        if not data or data.decode('utf-8')=='exit':
+            break
+        sock.send('Hello %s' %data.decode('utf-8'))
+    sock.close()
+    print('Connecting from %s is over'%addr)
